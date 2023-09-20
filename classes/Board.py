055c30c4b18e664d33e84f1bded802dc6cb9bdcc
Cloud <ruben.as532@gmail.com>
2023-09-20 20:31:27 +0200

    Added Piece on initial position

--- classes/Board.py
+++ classes/Board.py
@@ -1,20 +1,27 @@
+from classes.Piece import Piece
+
 class Board:
 
-    WHITE_SQUARE= chr(int('000' + '2B1C', 16))
+    EMPTY_SQUARE= chr(int('000' + '2B1C', 16))
+    BUSY_SQUARE = chr(int('000' + '2B1B', 16))
 
     def __init__(self,rows,columns):
         self.__rows = rows
         self.__columns = columns
+        self.__piece = Piece()
 
     def draw(self):  
         rows_count = 0
         while rows_count < self.__rows:
-            self.__drawDashesLine()
+            self.__drawRow(rows_count)
             rows_count += 1
 		
-    def __drawDashesLine(self):
-        columns_count = 0 
-        while columns_count < self.__columns:
-            print(self.WHITE_SQUARE,end="")
-            columns_count += 1
+    def __drawRow(self, current_row):
+        current_column = 0 
+        while current_column < self.__columns:
+            char_to_print = self.EMPTY_SQUARE
+            if self.__piece.hasPieceInPosition(current_column,current_row):
+                char_to_print = self.BUSY_SQUARE
+            print(char_to_print,end="")
+            current_column += 1
         print("")
--- classes/Piece.py
+++ classes/Piece.py
@@ -1,3 +1,14 @@
+from classes.Position import Position 
+
 class Piece:
-    def __init(self):
-        print("Piece!")
+    def __init__(self):
+        self.__positions = []
+        self.__positions.append(Position(4,0))
+        self.__positions.append(Position(4,1))
+        self.__positions.append(Position(5,1))
+        self.__positions.append(Position(6,1))
+
+    def hasPieceInPosition(self,x,y):
+        for position in self.__positions:
+            if position.x == x and position.y == y: return True  
+        return False
