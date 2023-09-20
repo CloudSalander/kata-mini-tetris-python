7750223efb7e31172e3b649d35ad7efaf440a940
Cloud <ruben.as532@gmail.com>
2023-09-20 19:11:23 +0200

    First version of Board Printing

--- classes/Board.py
+++ classes/Board.py
@@ -1,3 +1,17 @@
 class Board:
-    def __init(self):
-        print("Board!")
+    def __init__(self,rows,columns):
+        self.__rows = rows
+        self.__columns = columns
+
+    def draw(self):  
+        rows_count = 0
+        while rows_count < self.__rows:
+            self.__drawDashesLine()
+            rows_count += 1
+		
+    def __drawDashesLine(self):
+        columns_count = 0 
+        while columns_count < self.__columns:
+            print("_|",end="")
+            columns_count += 1
+        print("")
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from classes.Move import Move
+from classes.Board import Board
+BOARD_ROWS = 10
+BOARD_COLUMNS = 10
+
+
 move = Move.DOWN
+board = Board(BOARD_ROWS,BOARD_COLUMNS)
+board.draw()
+
 while move != 0:
     print("1-DOWN")
     print("2-RIGHT")
