05ad8b7c9b9da11c4b2d6b77ca126f2e51c589ed
Cloud <ruben.as532@gmail.com>
2023-09-22 17:33:32 +0200

    Solved non-numeric input bug

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def writeAvailableMoves():
         print(available_move)
 
 def isRightMove(move):
-    if move > 0 and move < 5: return True 
+    if (move.isnumeric()) and  int(move) >= 0 and int(move) < 5: return True 
     return False
 
 move = -1
 board = Board(BOARD_ROWS,BOARD_COLUMNS)
 board.play()
 
-while move != 0:
+while move != "0":
     writeAvailableMoves()
-    move = int(input("Enter movement"))
-    if(isRightMove(move)): 
-        board.play(Move(move))
+    move = input("Enter movement")
+    if(isRightMove(move) and move!= "0"): 
+            board.play(Move(int(move)))
